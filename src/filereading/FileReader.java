ddd8aca55b7b3ca0989f3f13e5a7ee846f5cc891
Olson Family <Olson Family@OlsonFamily-HP>
2017-09-25 23:38:17 -0400

    Done with 3 and 5, 1 is done but incorrectly

--- src/filereading/FileReader.java
+++ src/filereading/FileReader.java
@@ -13,6 +13,9 @@ import java.util.Scanner;
 public class FileReader {
 	
 	
+//	private static ArrayList<String> partsOfSpeech = new ArrayList<String>();
+	private static ArrayList<String> userWords = new ArrayList<String>();
+	
 	public static Scanner openFile(String f) {
 		
 		File file = new File(f);
@@ -63,6 +66,28 @@ public class FileReader {
 		return allLines;
 	}
 	
+	public static String checkBraces(String file) {
+		
+		int numHasToBeClosed = 0;
+
+		for (int i = 0; i < file.length(); i++) {
+			if (file.charAt(i) == '{') {
+				numHasToBeClosed++;
+
+			}
+			if (file.charAt(i) == '}') {
+				numHasToBeClosed--;
+
+			}
+		}
+		
+		if (numHasToBeClosed == 0)
+			return "Braces Balanced";
+		else
+			return "Braces not Balanced";
+		
+	}
+	
 	public static String isEqual(Path file1, Path file2) throws IOException {
 		
 		byte[] f1 = Files.readAllBytes(file1);
@@ -73,42 +98,109 @@ public class FileReader {
 			return "Files are not identical";
 	}
 	
-	public ArrayList<String> getArray (String f) {
+	public static ArrayList<String> pullPoS(String f) {
+		
+		ArrayList<String> pos = new ArrayList<String>();
+		
+		for (int i = 0; i < f.length(); i++) {
+			if (f.charAt(i) == '<') {
+				String a = f.substring(i, (f.indexOf(">", i) + 1));
+				pos.add(a);
+			}
+			
+		}
+		
+		
+		return pos;
+		
+	}
+	
+	public static ArrayList<String> getArray (ArrayList<String> partsOfSpeech) {
 		
 		ArrayList<String> words = new ArrayList<String>();
-		
+		Scanner input = new Scanner(System.in);
+		for (int i = 0; i < partsOfSpeech.size(); i++ ) {
+			
+			System.out.println("Please provide a(n)" + partsOfSpeech.get(i));
+			words.add(input.nextLine());
+			
+		}
 		
 		return words;
 	}
+
 	
-	public static void writeJava(PrintWriter output, String a) {
+	public static String fillStory (ArrayList<String> words, String story) {
 		
-		output.println(a);
-	}
-	
-	public static void fillStory (ArrayList<String> words) {
+		String filledStory = "";
+		ArrayList<String> storySections = new ArrayList<String>();
+		int a = 0;
+		for (int i = 0; i < story.length(); i++) {
+			
+			if (story.charAt(i) == '<') {
+				storySections.add(story.substring(a, i));
+				a = story.indexOf(">", i) + 1;
+			}
+			else if (i == story.length() - 1) 
+				storySections.add(story.substring(a, i));
+			
+		}
+		
+		if (storySections.size() > words.size()) {
+			for (int j = 0; j < storySections.size() - 1; j++) {
+				filledStory += storySections.get(j) + words.get(j);
+			}
+			filledStory += storySections.get(storySections.size() - 1);
+		}
+		else if (storySections.size() < words.size()) {
+			for (int j = 0; j < words.size() - 1; j++) {
+				filledStory += storySections.get(j) + words.get(j);
+			}
+			filledStory += words.get(words.size() - 1);
+		}
+		
+		return filledStory;
 		
 		
 	} 
+	
+	public static void writeJava(PrintWriter output, String brace, String equal, String story) {
+		
+		output.println(brace);
+		output.println("");
+		output.println(equal);
+		output.println("");
+		output.println(story);
+	}
 
 	
  	public static void main(String args[]) throws IOException {
 		
  		if (args.length < 2) {
-			System.out.println("You did not provide a file to print");
+			System.out.println("You did not provide enough files to use");
 			System.exit(1);
 		}
+ 			
+		
+		PrintWriter out = makeWriter(args[3]);
+		
 		Scanner in = openFile(args[2]);
 		if (in == null) System.exit(1);
-	//	Scanner file = openFile(args[3]);
-	//	String a = convToString(in);
-	//	System.out.println(a);
-		PrintWriter out = makeWriter(args[3]);
-		//printToFile(in, out);
+		String toOutputA = (checkBraces(convToString(in)));
 		Path file1 = Paths.get(args[2]);
 		Path file2 = Paths.get(args[4]);
 		String toOutputB = (isEqual(file1, file2));
-		writeJava(out, toOutputB);
+		
+		Scanner in2 = openFile(args[4]);
+		if (in2 == null) System.exit(1);
+		String storyPos = convToString(in2);
+		ArrayList<String> partsOfSpeech = pullPoS(storyPos);
+
+		ArrayList<String> userWords = getArray(partsOfSpeech);
+
+		String toOutputC = (fillStory(userWords, storyPos));
+		
+		writeJava(out, toOutputA, toOutputB, toOutputC);
 		in.close();
 		out.close();
 	}
